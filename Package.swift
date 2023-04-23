42faf43aeb21d0d16633786387dd174da14a281a
Jeytery <dimaostapchenko@gmail.com>
2023-04-23 14:01:00 +0300

    1

--- Package.swift
+++ Package.swift
@@ -20,9 +20,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "test_cloud",
-            dependencies: []),
-        .testTarget(
-            name: "test_cloudTests",
-            dependencies: ["test_cloud"]),
+            dependencies: [],
+            path: "Sources"
+        )
     ]
 )
